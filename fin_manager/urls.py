7454367fd708a08e45df6df7e816cd62ff10befa
Nikita Meshchaninov <nmm33342@gmail.com>
2022-04-08 18:29:45 +0000

    add jwt tokens

--- fin_manager/urls.py
+++ fin_manager/urls.py
@@ -13,8 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import include, path
+from django.urls import path
+from django.conf.urls.static import static
+
+from django.conf import settings
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
-    path("api-auth/", include("rest_framework.urls")),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
