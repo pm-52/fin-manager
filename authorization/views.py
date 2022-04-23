62a1a7aae87d09edd1c181d7efe90d8eece5e0cf
SpidWagon <vovacickin3386@gmail.com>
2022-04-23 21:41:11 +0300

    views changed

--- authorization/views.py
+++ authorization/views.py
@@ -1,25 +1,14 @@
 from rest_framework import permissions
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated, AllowAny
-# from rest_framework.generics import CreateAPIView, GenericAPIView
 from django.contrib.auth import get_user_model # If used custom user model
 
 from authorization.serializers import UserSerializer
 
 
-# class CreateUserView(CreateAPIView):
-
-#     model = get_user_model()
-#     permission_classes = [
-#         permissions.AllowAny # Or anon users can't register
-#     ]
-#     serializer_class = UserSerializer
-
-
 class CreateUserViewSet(viewsets.ModelViewSet):
     queryset = get_user_model().objects.all()
     serializer_class = UserSerializer
-    # permission_classes = [IsAuthenticated, permissions.AllowAny]
 
     def get_permissions(self):
         if self.request.method == 'POST':
